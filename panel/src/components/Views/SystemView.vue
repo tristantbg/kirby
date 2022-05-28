7f883cdd0dc661a54d39c3b5dda8748b5dcd5fe6
Lukas Bestle <account-github@lukasbestle.com>
2022-05-28 21:36:02 +0200

    Revert "Add console info message when system health checks ended"

--- panel/src/components/Views/SystemView.vue
+++ panel/src/components/Views/SystemView.vue
@@ -146,15 +146,14 @@ export default {
       );
     }
   },
-  async created() {
+  created() {
     console.info(
       "Running system health checks for the Panel system view; failed requests in the following console output are expected behavior."
     );
-    await this.check("content");
-    await this.check("git");
-    await this.check("kirby");
-    await this.check("site");
-    console.info("System health checks ended.");
+    this.check("content");
+    this.check("git");
+    this.check("kirby");
+    this.check("site");
   },
   methods: {
     async check(key) {
